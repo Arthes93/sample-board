719655e3c691b888eeca3e76e1cc94929ca7f64a
Dongwoo Yang <ckck803@gmail.com>
2021-02-27 01:43:52 +0900

    feat: @Profile을 통한 런타임 환경 설정
    - @Profile("prod")를 통해 운영환경에서 해당 bean을 생성하도록 설정한다.

--- src/main/java/com/example/sampleboard/database/DBConfigByYml.java
+++ src/main/java/com/example/sampleboard/database/DBConfigByYml.java
@@ -3,18 +3,19 @@ package com.example.sampleboard.database;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
 
 import javax.sql.DataSource;
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Configuration
+@Profile("prod")
+//@PropertySource(value = "classpath:application-h2.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
 @PropertySource(value = "classpath:application-mysql.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
-@PropertySource(value = "classpath:application-h2.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
 public class DBConfigByYml {
 
     @Value("${spring.datasource.driver-class-name}")
@@ -38,9 +39,4 @@ public class DBConfigByYml {
                 .password(password)
                 .build();
     }
-
-//    @Override
-//    public TransactionManager annotationDrivenTransactionManager() {
-//        return new DataSourceTransactionManager(dataSource());
-//    }
 }
--- src/test/java/com/example/sampleboard/SampleBoardApplicationTests.java
+++ src/test/java/com/example/sampleboard/SampleBoardApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.sampleboard;
 
+import com.example.sampleboard.database.YamlPropertySourceFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class SampleBoardApplicationTests {
@@ -9,5 +12,4 @@ class SampleBoardApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
